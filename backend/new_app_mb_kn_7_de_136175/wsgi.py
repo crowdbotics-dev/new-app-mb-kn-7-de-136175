720829f4fda1429a2d8cb8b371c88ee42832ea45
crowdbotics-dev <engineering@crowdbotics.com>
2024-07-04 19:04:51 +0000

    Update models

--- backend/new_app_mb_kn_7_de_136175/wsgi.py
+++ backend/new_app_mb_kn_7_de_136175/wsgi.py
@@ -11,8 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault(
-    "DJANGO_SETTINGS_MODULE", "new_app_mb_kn_7_de_136175.settings"
-)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "new_app_mb_kn_7_de_136175.settings")
 
 application = get_wsgi_application()
